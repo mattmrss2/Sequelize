4cc14b87a17f12265e90ee974453a0b6bf417131
mattmrss2 <78233113+mattmrss2@users.noreply.github.com>
2021-03-29 19:25:45 -0400

    L

--- public/script.js
+++ public/script.js
@@ -1,6 +1,17 @@
 
+
 async function swag() {
-    const diningHalls = await fetch("/dining/:hall_id")
-    console.log(diningHalls)
+    const diningHalls = await fetch("/api/dining");
+    const assman = await diningHalls.json();
+    const resTable = document.querySelector(".tableBody");
+    console.log(resTable)
+    console.log(assman);
+    
+    assman.data.forEach((item) => {
+        console.log(item)
+        const appendItem = document.createElement('tr');
+        appendItem.innerHTML = `<td>${item.hall_id}</td><td>${item.hall_name}</td><td>${item.hall_address}</td>`
+        resTable.append(appendItem)
+    });
 }
 window.onload = swag;
