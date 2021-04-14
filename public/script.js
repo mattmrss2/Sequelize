c8ca8b44408c1d53e67e0709c927675738a43579
mattmrss2 <78233113+mattmrss2@users.noreply.github.com>
2021-04-13 20:46:28 -0400

    finish

--- public/script.js
+++ public/script.js
@@ -1,4 +1,75 @@
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
 
+async function mealsMacros() {
+    const meals = await fetch("/api/meals");
+
+    const mealJson = await meals.json();
+    
+
+    const macros = await fetch("/api/macros")
+    console.log(macros, mealJson)
+
+    const macrosJson = await macros.json()
+    console.log(macrosJson)
+
+    let takenNum = [];
+    let mealList = [];
+    let carbList= [];
+    let fatList = [];
+    let protList = [];
+    let numdeb = [];
+
+    
+    for(i=0;i < 10; i++) {
+        mealDict = {}
+        randInt = getRandomInt(mealJson.length)
+        numdeb.push(randInt)
+        
+        if (mealList.indexOf(mealJson[randInt]) !== -1) {
+            i--
+            continue
+        }
+        else {
+            
+            takenNum.push(randInt);
+            mealList.push(mealJson[randInt]);
+            carbList.push({label: mealJson[randInt].meal_name, y: macrosJson[randInt].carbs})
+            fatList.push({label: mealJson[randInt].meal_name, y: macrosJson[randInt].fat})
+            protList.push({label: mealJson[randInt].meal_name, y: macrosJson[randInt].protein})
+        
+        }
+        
+    }; 
+    console.log(carbList,fatList,protList,mealList,macrosJson)
+    var mealChart = new CanvasJS.Chart("chartContainer", {
+        theme:"light1",
+        animationEnabled: true,
+        title: {
+            text: "Meals Chart"
+        },
+        data: [{
+            type: "stackedBar",
+            name: "carbs",
+            showInLegend: "true",
+            dataPoints: carbList
+            },{
+            type:"stackedBar",
+            name:"fats",
+            showInLegend: "true",
+            dataPoints: fatList
+            },{
+                type:"stackedBar",
+                name: "protein",
+                showInLegend: "true",
+                dataPoints: protList
+            }
+
+        ]
+    })
+mealChart.render()
+}
 
 async function diningTable() {
     const diningHalls = await fetch("/api/dining");
@@ -14,4 +85,4 @@ async function diningTable() {
         resTable.append(appendItem)
     });
 }
-window.onload = diningTable;
+window.onload = mealsMacros;
